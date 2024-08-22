6ea0651acb576d180a5f2965e606cde9d31f6abe
Dario Fabricius <andre.fabricius@gmail.com>
2024-08-22 14:09:25 +0200

    Fundamentals 2 2

---
+++ 21_switch/SwitchTest.java
@@ -0,0 +1,21 @@
+public class SwitchTest{
+	public static void main(String[] args){
+		int a = 4;
+
+		switch(a){
+		case 1 :{
+			System.out.println("Cool");
+		}break;
+		case 2 : {
+			System.out.println("Cool");
+		}break;
+		case 3 : {
+			System.out.println("Cool");
+		}break;
+		default : {
+			System.out.println("Not cool");
+		}break;
+
+		}
+	}
+}
