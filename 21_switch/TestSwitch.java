8eaa08720b2b8103f71d323154c3656639121e67
Dario Fabricius <andre.fabricius@gmail.com>
2024-09-11 15:04:08 +0200

    Conditional exams

---
+++ 21_switch/TestSwitch.java
@@ -0,0 +1,16 @@
+public class TestSwitch{
+	public static void main(String[] args){
+
+		int a = 3;
+
+		if(10 < 5)
+		if(10 < 15)System.out.println("5" + " " + a);
+		else; {
+
+			System.out.println("Else ");
+		}
+
+
+
+	}
+}
