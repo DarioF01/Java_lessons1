e0bd35198f68a0102fba6195277e4fdd6ae8b4b2
DarioF01 <andre.fabricius@gmail.com>
2024-12-05 14:58:44 +0200

    return

---
+++ 21_switch/Test1.java
@@ -0,0 +1,6 @@
+public class Test1{
+	public static void main(String[] args){
+		double a = 5, b = 3;
+		System.out.println(a+b+Math.pow(a, b));
+	}
+}
---
+++ 23_arrays/ArraysTest1.java
@@ -0,0 +1,7 @@
+public class ArraysTest1{
+	public static void main(String[] args){
+		long [] lottonumbers = new long [6];
+
+		System.out.println("Value = " + lottonumbers[0]);
+	}
+}
